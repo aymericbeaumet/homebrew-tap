1fa547b5e7ad6d763cb25e435a2b55ebc49e9a6e
Aymeric Beaumet <aymeric@beaumet.me>
2026-05-18 00:27:50 +0200

    squeeze-nightly nightly-20260517-38d59c4

--- Formula/squeeze-nightly.rb
+++ Formula/squeeze-nightly.rb
@@ -3,9 +3,9 @@
 class SqueezeNightly < Formula
   desc "Extract rich information from any text (URIs, codetags, etc.) - nightly"
   homepage "https://github.com/aymericbeaumet/squeeze"
-  url "https://github.com/aymericbeaumet/squeeze/archive/ab79e4d649f5ab067b7e5dd83a9d6f33e195fb80.tar.gz"
-  version "nightly-20260517-ab79e4d"
-  sha256 "98b56bafda625d8aaebb0d1c560c67dee984202c56a0c844031e472e41ef46b8"
+  url "https://github.com/aymericbeaumet/squeeze/archive/38d59c455d82502846402ac07e5766a1c09c9221.tar.gz"
+  version "nightly-20260517-38d59c4"
+  sha256 "79b3d26fec0fe3c472419ae8fe2441ff4c91a5a66c15e85be66fbfa5be92e120"
   license "MIT"
 
   depends_on "rust" => :build
@@ -13,6 +13,7 @@ class SqueezeNightly < Formula
   conflicts_with "squeeze", because: "squeeze-nightly and squeeze install the same binary"
 
   def install
+    ENV["SQUEEZE_VERSION"] = version.to_s
     system "cargo", "install", *std_cargo_args(path: "squeeze-cli")
   end
 
