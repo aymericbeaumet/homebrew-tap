b4149d9f38d9ab81d90e7a83f502a8fce593d03c
Aymeric Beaumet <aymeric@beaumet.me>
2026-09-05 01:27:29 +0200

    chore: bump aggr formula to 1.4.1

--- Formula/aggr.rb
+++ Formula/aggr.rb
@@ -1,8 +1,8 @@
 class Aggr < Formula
   desc "Git-native feed reader: aggr.toml in, static site out, history on a branch"
   homepage "https://github.com/aymericbeaumet/aggr"
-  url "https://github.com/aymericbeaumet/aggr/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "2a26f89762b1a920b9d3c82de4f28114cce95cf639f936d3cbdab892ef3f1012"
+  url "https://github.com/aymericbeaumet/aggr/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "022a060e5528f121c7236660f8e4101dd1af75aa229ca52b24eefcab2efbab9b"
   license "MIT"
   head "https://github.com/aymericbeaumet/aggr.git", branch: "main"
 
