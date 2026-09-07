ed827320f15fbb426567cb28edeedfcecd35629a
Aymeric Beaumet <aymeric@beaumet.me>
2026-09-07 18:31:24 +0200

    chore: update aggr to v1.6.0

--- Formula/aggr.rb
+++ Formula/aggr.rb
@@ -1,8 +1,8 @@
 class Aggr < Formula
   desc "Git-native feed reader: aggr.toml in, static site out, history on a branch"
   homepage "https://github.com/aymericbeaumet/aggr"
-  url "https://github.com/aymericbeaumet/aggr/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "022a060e5528f121c7236660f8e4101dd1af75aa229ca52b24eefcab2efbab9b"
+  url "https://github.com/aymericbeaumet/aggr/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "bf57bc59bef80390388250b540e6683127e2b05ff7e7357d4ccb24c32aa43993"
   license "MIT"
   head "https://github.com/aymericbeaumet/aggr.git", branch: "main"
 
