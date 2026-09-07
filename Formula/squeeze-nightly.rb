efcfd35402ed09d50c6207b2c439d384b2f4ba6e
Aymeric Beaumet <aymeric@beaumet.me>
2026-09-07 18:31:17 +0200

    squeeze-nightly nightly-20260907-1d0f8ee

--- Formula/squeeze-nightly.rb
+++ Formula/squeeze-nightly.rb
@@ -3,9 +3,9 @@
 class SqueezeNightly < Formula
   desc "Extract rich information from any text (URIs, codetags, etc.) - nightly"
   homepage "https://github.com/aymericbeaumet/squeeze"
-  url "https://github.com/aymericbeaumet/squeeze/archive/66610bba4c550e76b5dc9404500ee966a4b97502.tar.gz"
-  version "nightly-20260907-66610bb"
-  sha256 "4ef7f88d00e6db13e4f2aa5042d3cd49d2b8cce95ce648b7e74f085df6d58fe4"
+  url "https://github.com/aymericbeaumet/squeeze/archive/1d0f8ee4c3fc1b224081418951eea29f249f8600.tar.gz"
+  version "nightly-20260907-1d0f8ee"
+  sha256 "516b288ffcec5873caf2099e4bac10ef602b0ff07837cd2cbfbcdd8fa948f1ed"
   license "MIT"
 
   depends_on "rust" => :build
